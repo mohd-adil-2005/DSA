2daedfe64ea702be6d120a566aac88963ef2cf59
mohd Adil <mohdadil.tech@gmail.com>
2026-02-19 17:55:35 +0530

    catalans concepts

--- catalans/Catalans.java
+++ catalans/Catalans.java
@@ -1,9 +1,17 @@
-
-
-public class Catalans {
-    
+class Catalans {
+    public static int catalans(int n){
+      if(n==0|| n==1){
+        return 1;
+      }
+      int ans =0;
+      for(int i=0; i<n; i++){
+        ans+=catalans(i)*catalans(n-i-1);
+      }
+      return ans;
+    }
   public static void main(String[] args){
       System.out.println("hello , catalans concept");
+      System.out.println(catalans(5));
   }
 
 }
