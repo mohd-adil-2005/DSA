95f9b91cfef513634e79523c2b97991ba1e0efcc
mohd Adil <mohdadil.tech@gmail.com>
2026-02-21 22:57:42 +0530

    kahan's algo

--- Graph/classroom.java
+++ Graph/classroom.java
@@ -15,20 +15,52 @@ public class Classroom{
     }
 
 
+    public static void topsort(ArrayList<Edge>[] graph){
+        int inddeg[]= new int[graph.length];
+         for(int i=0; i<graph.length; i++){
+            for(int j=0; j<graph[i].size(); j++){
+                Edge e = graph[i].get(j);
+                inddeg[e.dest]++;
+            }
+         }
+
+         Queue<Integer> q= new LinkedList<>();
+         for(int i=0; i<graph.length; i++){
+            if(inddeg[i]==0){
+                q.add(i);
+            }
+         }
+         while(!q.isEmpty()){
+            int curr= q.remove();
+            System.out.println("visting toposrt nodes " + curr);
+                 for(int i=0; i<graph[curr].size(); i++){
+                      Edge e= graph[curr].get(i);
+                      inddeg[e.dest]--;
+                      if(inddeg[e.dest]==0){
+                        q.add(e.dest);
+                      }
+                 }
+         }
+       
+    }
+
+
     public static void createGraph(ArrayList<Edge>[] graph){
         for(int i=0; i<graph.length; i++){
             graph[i]= new ArrayList<>();
         }
-        graph[0].add(new Edge(0,1,5));
-        graph[0].add(new Edge(0,2,1));
+        // graph[0].add(new Edge(0,1,5));
+        // graph[0].add(new Edge(0,2,1));
 
-        graph[1].add(new Edge(1,0,3));
-        graph[1].add(new Edge(1,3,1));
-        graph[2].add(new Edge(2,0,1));
-        graph[2].add(new Edge(2,3,3));
-        
-        graph[3].add(new Edge(3,1,1));
-        graph[3].add(new Edge(3,2,2));
+          graph[5].add(new Edge(5, 0, 1));
+        graph[5].add(new Edge(5, 2, 1));
+
+        graph[4].add(new Edge(4, 0, 1));
+        graph[4].add(new Edge(4, 1, 1));
+
+        graph[2].add(new Edge(2, 3, 1));
+
+        graph[3].add(new Edge(3, 1, 1));
     }
 
 
@@ -96,11 +128,12 @@ public class Classroom{
     }
     public static void main(String[] args){
         System.out.println("Hello, Graph");
-        int v=4;
+        int v=6;
         @SuppressWarnings("unchecked")
        ArrayList<Edge>[] graph = new ArrayList[v];
         createGraph(graph);
         BfsAlgo(graph);
         System.out.println("the Graph that is given is Bipartite:"+isBipartite(graph));
+        topsort(graph);
     }
 }
